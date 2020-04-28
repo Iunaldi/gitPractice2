94cb8102f5f83e3448da5fb5c177794ed515cf07
Iunaldi <64110457+Iunaldi@users.noreply.github.com>
2020-04-28 16:23:56 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,6 +4,7 @@ public class HappyBirthday {
     public static void main(String[] args) {
         
         System.out.println("Happy Birdday");
+        System.out.println("Happy Birdday");
 
     }
 }
